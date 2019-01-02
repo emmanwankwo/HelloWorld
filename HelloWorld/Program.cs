ed253312b1dfe2f2184029f82f63b93c6dc4d01e
Emmanuel <emmanuelnwankwo@hotmail.com>
2019-01-02 22:44:19 +0100

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 0;
+            int number2 = 0;
+            int sum = 0;
+
+            Console.WriteLine("Enter the first number: ");
+            number1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            number2 = Int32.Parse(Console.ReadLine());
+
+            sum = number1 + number2;
+            Console.WriteLine("The total sum of the value are: " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
